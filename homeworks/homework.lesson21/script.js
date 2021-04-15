7a6432901ac0ceec7f440a2b62af9de2a04d5489
Vlad Balabash <vladb951@gmail.com>
2021-04-15 23:47:36 +0300

    lesson 23

--- homeworks/homework.lesson21/script.js
+++ homeworks/homework.lesson21/script.js
@@ -1,10 +1,11 @@
 $(function () {
-    const todoItems = [];
+    let todoItems = [];
 
-    function addItem(value) {
+    function addItem(value, id = Date.now()) {
         todoItems.push({
-            id: Date.now(),
-            title: value
+            id,
+            title: value,
+            completed: false
         });
     }
 
@@ -16,6 +17,29 @@ $(function () {
         renderItems(todoItems);
     }
 
+    function onItemCheckout(id) {
+        const index = todoItems.findIndex(e => e.id == id);
+        todoItems[index].completed = !todoItems[index].completed;
+        renderItems(todoItems);
+
+        // todoItems = todoItems.filter(e => e.id != id)
+        // renderItems(todoItems);
+
+        fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(todoItems[index]),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        }).then(e => {
+            alert('Item was updated successfully!');
+        }).catch(e => {
+            todoItems[index].completed = !todoItems[index].completed;
+            renderItems(todoItems);
+            alert('Something went wrong');
+        });
+    }
+
     function init() {
         $('#add-btn').on('click', onAddItem);
         $('#todo-input').on('keypress', e => {
@@ -24,26 +48,40 @@ $(function () {
             }
         });
 
-        // fetch('https://jsonplaceholder.typicode.com/todos')
-        //     .then(response => response.json())
-        //     .then(json => {
-        //         json.slice(0, 10).forEach(e => {
-        //             addItem(e.title);
-        //         });
-        //         renderItems(todoItems);
-        //     });
-        $('#list').on('click', '.list-group-item', console.log);
+        loadToDoList();
+        $('#list').on('click', '.list-group-item .form-check-input', e => {
+            const id = $(e.currentTarget).parent().parent().attr('data-id');
+            onItemCheckout(id);
+        });
     }
 
     function renderItems(items) {
         const $list = $('#list');
         $list.empty();
 
-        items.forEach(e => {
-            const $li = $(`<li class="list-group-item" data-id="${e.id}">${e.title}</li>`);
+        items.forEach((e, i) => {
+            const $li = $(`<li class="list-group-item d-flex ${e.completed ? 'completed' : ''}" data-id="${e.id}">
+                <div class="form-check ms-2">
+                    <input type="checkbox" class="form-check-input" ${e.completed ? 'checked' : ''}>
+                </div>
+                <div class="ms-2 me-auto">
+                   ${i + 1} ${e.title}
+                </div>
+            </li>`);
             $list.append($li);
 
         });
+    }
+
+    function loadToDoList() {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => {
+                json.slice(0, 10).forEach(e => {
+                    addItem(e.title, e.id);
+                });
+                renderItems(todoItems);
+            });
     }
 
     init();
--- lessons/lesson22/script.js
+++ lessons/lesson22/script.js
@@ -1,57 +1,190 @@
-// ES2015 or ES6
+// // ES2015 or ES6
 
-// 1 - class -> constructor
-// 2 - () => / arrow function
-// 3 - Promise
-// 4 - let/const
-// 5 - ... (spread/rest)
+// // 1 - class -> constructor
+// // 2 - () => / arrow function (closure context, no "arguments", can't use with "new")
+// // 3 - Promise
+// // 4 - let/const (block scope)
+// // 5 - ... (spread/rest)
 
-// const resultArray = [1, 2, 3, ...arr1, ...arr2]; // <- spread array
-// const resultObject = { ...obj1, name: 'Alex' }; // <- spread object (clone)
+// // const resultArray = [1, 2, 3, ...arr1, ...arr2]; // <- spread array
+// // const resultObject = { ...obj1, name: 'Alex' }; // <- spread object (clone)
 
-// const foo = (a, ...params) => { // rest - params = [3, true]
+// // const foo = (a, ...params) => { // rest - params = [3, true]
 
+// // };
+
+// // foo(1, 3, true);
+
+// // 6 - destructurisation
+// const obj = {
+//     name: 'Bob',
+//     age: 42,
+//     country: 'UK',
+//     city: {
+//         name: 'London',
+//         population: '12m'
+//     }
 // };
 
-// foo(1, 3, true);
+// // var country = obj.country; // ES5
 
-// 6 - destructurisation
-const obj = {
-    name: 'Bob',
+// const { country: someCountry, city: { population } } = obj; // ES6
+// console.log(someCountry, population);
+
+// const arr1 = [true, 33, 42];
+
+// const [, age] = arr1;
+
+// const [, ...arr2] = arr1;
+
+// console.log(age, arr2);
+
+// // 7 - default argument value
+
+// // ES5
+// function setName(name) {
+//     if (!name) {
+//         name = 'John';
+//     }
+//     user.name = name;
+// }
+
+// // ES6
+// function setName(name = 'John', age, sex) {
+//     user.name = name;
+// }
+
+// setName(undefined, 42, 'Male');
+
+// // 8 - object assignment
+// const id = Date.now();
+
+// // ES5
+// var _todItem = {
+//     id: id
+// };
+
+// // ES6
+// const todoItem = {
+//     id
+// };
+
+// // 9 - template string
+// const str = `${true ? 'test' : 'foo'} djf kfdjfdjk`;
+
+// // 10 - async/await
+
+// // Promise way
+// function foo() {
+//     let response;
+
+//     fetch('https://google.com')
+//         .then(resp => {
+//             // some code
+//             response = resp;
+//             return fetch('https://google.com/search');
+//         })
+//         // .catch(e => {
+//         //     // error handler
+//         //     return fetch('https://google.com/search');
+//         // })
+//         .then(resp => {
+//             return fetch('https://google.com/search?q=hillel');
+//         })
+//         // .catch(e => {
+//         //     // error handler
+//         //     return fetch('https://google.com/search?q=hillel');
+//         // })
+//         .then(e => {
+//             // some code
+//         })
+//         .catch(e => {
+//             // 
+//         });
+
+
+//     console.log(response); // undefined
+// }
+
+// async function bar() {
+//     try {
+//         let response, searchResponse, qResponse;
+//         try {
+//             response = await fetch('https://google.com');
+//         } catch (e) {
+//             // error handling
+//         }
+//         try {
+//             searchResponse = await fetch('https://google.com/search');
+//         } catch (e) {
+//             // error handling
+//         }
+//         try {
+//             qResponse = await fetch('https://google.com/search?q=hillel');
+//         } catch (e) {
+//             // error handling
+//         }
+
+//         console.log(response);
+//         return response;
+//     } catch (e) {
+//         console.error(e);
+//     }
+// }
+
+// const r = bar();
+
+// function buz() {
+//     try {
+//         return 42;
+//     } catch (e) {
+
+//     } finally {
+//         console.log('hello from finally!');
+//     }
+// }
+
+// const a = buz();
+// console.log(a);
+
+// 11 - generators
+
+// function *demo() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+// }
+
+// const a = demo();
+// console.log(a.next());
+// console.log(a.next());
+// console.log(a.next());
+// console.log(a.next());
+// console.log(a.next());
+
+const newObj = {
+    name: 'Test',
     age: 42,
-    country: 'UK',
-    city: {
-        name: 'London',
-        population: '12m'
+    iterate: function *iterate() {
+        yield this.name;
+        yield this.age;
     }
 };
 
-// var country = obj.country; // ES5
+let done = false;
+const objGen = newObj.iterate();
+do {
+    const { value, done: _done } = objGen.next();
+    console.log(value);
+    done = _done;
+} while(!done);
 
-const { country: someCountry, city: { population } } = obj; // ES6
-console.log(someCountry, population);
-
-const arr1 = [true, 33, 42];
-
-const [, age] = arr1;
-
-const [, ...arr2] = arr1;
-
-console.log(age, arr2);
-
-// 7 - default argument value
-
-// ES5
-function setName(name) {
-    if (!name) {
-        name = 'John';
-    }
-    user.name = name;
+for(let e in newObj) {
+    console.log(e);
 }
 
-// ES6
-function setName(name = 'John', age, sex) {
-    user.name = name;
-}
+// 12 - BigInt, Symbol
 
-setName(undefined, 42, 'Male');
+// BigInt
+
+// Symbol
